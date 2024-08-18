94ecdb21dfa2ebd5c5124575dbefc3eedcd94840
Marcia Ong <MrMarciaOng@gmail.com>
2024-08-18 22:35:22 +0800

    update text and css

--- src/app/components/Web3AuthComponent.tsx
+++ src/app/components/Web3AuthComponent.tsx
@@ -1,29 +1,38 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
-import { Web3Auth } from '@web3auth/modal';
-import { CHAIN_NAMESPACES, IProvider, WEB3AUTH_NETWORK } from '@web3auth/base';
-import Web3 from 'web3';
-import { EthereumPrivateKeyProvider } from '@web3auth/ethereum-provider';
-import { CommonPrivateKeyProvider } from '@web3auth/base-provider';
-import { OpenloginAdapter } from '@web3auth/openlogin-adapter';
-import { MetamaskAdapter } from '@web3auth/metamask-adapter';
-import { SafeFactory, SafeAccountConfig } from '@safe-global/protocol-kit';
+import { useEffect, useState } from "react";
+import { Web3Auth } from "@web3auth/modal";
+import { CHAIN_NAMESPACES, IProvider, WEB3AUTH_NETWORK } from "@web3auth/base";
+import Web3 from "web3";
+import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
+import { CommonPrivateKeyProvider } from "@web3auth/base-provider";
+import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
+import { MetamaskAdapter } from "@web3auth/metamask-adapter";
+import { SafeFactory, SafeAccountConfig } from "@safe-global/protocol-kit";
 
-const clientId = 'BJ-uOTw1stDYuZ7PighbkKr9UyR-_nzDmkrM87BbI9GCFb_JG215OCMXmy9-V2hTCTlICGmzP09zHFug0FxwMH4';
+const clientId =
+  "BJ-uOTw1stDYuZ7PighbkKr9UyR-_nzDmkrM87BbI9GCFb_JG215OCMXmy9-V2hTCTlICGmzP09zHFug0FxwMH4";
 
 const chainConfig = {
-  chainId: '0xaa36a7',
-  rpcTarget: 'https://eth-sepolia.public.blastapi.io',
+  chainId: "0xaa36a7",
+  rpcTarget: "https://eth-sepolia.public.blastapi.io",
   chainNamespace: CHAIN_NAMESPACES.EIP155,
-  displayName: 'Sepolia Testnet',
-  blockExplorerURL: 'https://sepolia.etherscan.io/',
+  displayName: "Sepolia Testnet",
+  blockExplorerURL: "https://sepolia.etherscan.io/",
 };
+
+// const chainConfig = {
+//   chainId: "0x66eee",
+//   rpcTarget: "https://sepolia-rollup.arbitrum.io/rpc",
+//   chainNamespace: CHAIN_NAMESPACES.EIP155,
+//   displayName: "Arbitrum Sepolia Testnet",
+//   blockExplorerURL: "https://sepolia.arbiscan.io/",
+// };
 
 const metamaskAdapter = new MetamaskAdapter({
   clientId,
   sessionTime: 3600,
-  web3AuthNetwork: 'sapphire_mainnet',
+  web3AuthNetwork: "sapphire_mainnet",
   chainConfig: chainConfig,
 });
 
@@ -43,7 +52,7 @@ export default function Web3AuthComponent() {
   const [provider, setProvider] = useState<IProvider | null>(null);
   const [loggedIn, setLoggedIn] = useState(false);
   const [showModal, setShowModal] = useState(false);
-  const [secondWalletAddress, setSecondWalletAddress] = useState('');
+  const [secondWalletAddress, setSecondWalletAddress] = useState("");
 
   useEffect(() => {
     const init = async () => {
@@ -79,7 +88,7 @@ export default function Web3AuthComponent() {
     await web3auth.logout();
     setProvider(null);
     setLoggedIn(false);
-    uiConsole('logged out');
+    uiConsole("logged out");
   };
 
   const openModal = () => {
@@ -101,7 +110,7 @@ export default function Web3AuthComponent() {
 
   const getAccounts = async () => {
     if (!provider) {
-      uiConsole('provider not initialized yet');
+      uiConsole("provider not initialized yet");
       return;
     }
     const web3 = new Web3(provider as any);
@@ -111,99 +120,106 @@ export default function Web3AuthComponent() {
 
   const getBalance = async () => {
     if (!provider) {
-      uiConsole('provider not initialized yet');
+      uiConsole("provider not initialized yet");
       return;
     }
     const web3 = new Web3(provider as any);
     const address = (await web3.eth.getAccounts())[0];
-    const balance = web3.utils.fromWei(await web3.eth.getBalance(address), 'ether');
+    const balance = web3.utils.fromWei(
+      await web3.eth.getBalance(address),
+      "ether"
+    );
     uiConsole(balance);
   };
 
-  const verifySignedMessage = async (originalMessage: string, signedMessage: string) => {
+  const verifySignedMessage = async (
+    originalMessage: string,
+    signedMessage: string
+  ) => {
     if (!provider) {
-      uiConsole('provider not initialized yet');
+      uiConsole("provider not initialized yet");
       return;
     }
     const web3 = new Web3(provider as any);
-    const recoveredAddress = await web3.eth.personal.ecRecover(originalMessage, signedMessage);
+    const recoveredAddress = await web3.eth.personal.ecRecover(
+      originalMessage,
+      signedMessage
+    );
     uiConsole(`Recovered Address: ${recoveredAddress}`);
   };
 
   const signMessage = async () => {
     if (!provider) {
-      uiConsole('provider not initialized yet');
+      uiConsole("provider not initialized yet");
       return;
     }
     const web3 = new Web3(provider as any);
     const fromAddress = (await web3.eth.getAccounts())[0];
-    const txObject = {
-      from: fromAddress,
-      to: '0x4ce32cCb81F83C447d9AD1543cE72976b4Ef243D',
-      value: web3.utils.toWei('0.01', 'ether'),
-      gas: 21000,
-    };
-    const txHash = web3.utils.soliditySha3(
-      { type: 'address', value: txObject.from },
-      { type: 'address', value: txObject.to },
-      { type: 'uint256', value: txObject.value },
-      { type: 'uint256', value: txObject.gas }
-    );
+    const message_to_sign = "Hello World";
     try {
-      const signedMessage = await web3.eth.personal.sign(txHash as string, fromAddress, '');
+      const signedMessage = await web3.eth.personal.sign(
+        message_to_sign as string,
+        fromAddress,
+        ""
+      );
       uiConsole(`Signed Message: ${signedMessage}`);
-      await verifySignedMessage(txHash as string, signedMessage);
+      await verifySignedMessage(message_to_sign as string, signedMessage);
     } catch (error: any) {
       uiConsole(`Transaction signing failed: ${error.message}`);
     }
   };
 
   function stringifyWithBigInt(obj: any) {
-    return JSON.stringify(obj, (key, value) => (typeof value === 'bigint' ? value.toString() : value));
+    return JSON.stringify(obj, (key, value) =>
+      typeof value === "bigint" ? value.toString() : value
+    );
   }
 
   async function createSafeTransaction() {
     if (!provider) {
-      uiConsole('provider not initialized yet');
+      uiConsole("provider not initialized yet");
       return;
     }
     const web3 = new Web3(provider as any);
     const address = (await web3.eth.getAccounts())[0];
     const safeFactory = await SafeFactory.init({ provider: provider });
-    uiConsole('signed in with address: ', address);
-    uiConsole('current signer user address: ', address);
-    uiConsole('secondWalletAddress: ', secondWalletAddress);
+    uiConsole("signed in with address: ", address);
+    uiConsole("current signer user address: ", address);
+    uiConsole("secondWalletAddress: ", secondWalletAddress);
     const safeAccountConfig: SafeAccountConfig = {
       owners: [address, secondWalletAddress],
       threshold: 2,
     };
     const nonce = await web3.eth.getTransactionCount(address);
-    uiConsole('Nonce: ', nonce.toString());
+    uiConsole("Nonce: ", nonce.toString());
 
     const saltNonce = nonce.toString();
-    const predictedSafeAddress = await safeFactory.predictSafeAddress(safeAccountConfig, saltNonce);
-    uiConsole('Predicted Safe Address: ', predictedSafeAddress);
+    const predictedSafeAddress = await safeFactory.predictSafeAddress(
+      safeAccountConfig,
+      saltNonce
+    );
+    uiConsole("Predicted Safe Address: ", predictedSafeAddress);
     try {
       const protocolKitOwner = await safeFactory.deploySafe({
         safeAccountConfig,
         saltNonce: saltNonce,
         callback: (txHash: string) => {
-          uiConsole('txHash: ', txHash);
+          uiConsole("txHash: ", txHash);
         },
       });
       const safeAddress = await protocolKitOwner.getAddress();
-      uiConsole('Your Safe has been deployed:');
+      uiConsole("Your Safe has been deployed:");
       uiConsole(`https://sepolia.etherscan.io/address/${safeAddress}`);
       uiConsole(`https://app.safe.global/sep:${safeAddress}`);
     } catch (error) {
-      uiConsole('Error: ', error);
+      uiConsole("Error: ", error);
     }
   }
 
   function uiConsole(...args: any[]): void {
-    const consoleElement = document.querySelector('#console');
+    const consoleElement = document.querySelector("#console");
     if (consoleElement) {
-      const newMessage = document.createElement('p');
+      const newMessage = document.createElement("p");
       newMessage.innerHTML = JSON.stringify(args || {}, null, 2);
       consoleElement.insertBefore(newMessage, consoleElement.firstChild);
     }
@@ -211,9 +227,9 @@ export default function Web3AuthComponent() {
   }
 
   function clearLog() {
-    const consoleElement = document.querySelector('#console');
+    const consoleElement = document.querySelector("#console");
     if (consoleElement) {
-      consoleElement.innerHTML = '';
+      consoleElement.innerHTML = "";
     }
   }
 
@@ -237,12 +253,14 @@ export default function Web3AuthComponent() {
         </div>
         <div>
           <button onClick={openModal} className="card">
-            Create Safe Transaction
+            Create Safe Smart Account
           </button>
           {showModal && (
             <div className="modal">
               <div className="modal-content">
                 <h2>Enter Second Wallet Address</h2>
+                <i>Signer and 2nd address will be co-owners of safe smart account</i>
+                <br />
                 <input
                   type="text"
                   value={secondWalletAddress}
@@ -254,6 +272,11 @@ export default function Web3AuthComponent() {
               </div>
             </div>
           )}
+        </div>
+        <div>
+          <button onClick={signMessage} className="card">
+            Sign Message
+          </button>
         </div>
         <div>
           <button onClick={clearLog} className="card">
@@ -278,8 +301,8 @@ export default function Web3AuthComponent() {
   return (
     <div>
       <div className="grid">{loggedIn ? loggedInView : unloggedInView}</div>
-      <div id="console" style={{ whiteSpace: 'pre-line' }}>
-        <p style={{ whiteSpace: 'pre-line' }}></p>
+      <div id="console" style={{ whiteSpace: "pre-line" }}>
+        <p style={{ whiteSpace: "pre-line" }}></p>
       </div>
     </div>
   );
